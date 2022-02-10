a43cc232e107c15b8738d36ee65af337719dfcdf
Kim Kinnear <kim@kinnear.com>
2022-02-10 15:43:38 -0500

    Fix zprint version used.

--- project.clj
+++ project.clj
@@ -10,6 +10,6 @@
   :eval-in-leiningen true
   :jvm-opts ^:replace ["-server" "-Xss500m"]
   :profiles {:dev {:dependencies [[expectations "2.2.0-rc3"]]}}
-  :dependencies [[zprint "1.2.1"]
+  :dependencies [[zprint "1.2.2"]
                  #_[clojure-future-spec "1.9.0-alpha17"]
                  [me.raynes/fs "1.4.6"]])
