0f225855dc76fac2041a77bde671e13ceade663f
Kim Kinnear <kim@kinnear.com>
2023-10-15 12:27:47 -0400

    New version.

--- src/leiningen/zprint.clj
+++ src/leiningen/zprint.clj
@@ -7,7 +7,7 @@
 (defn lein-zprint-about
   "Return version of this program."
   []
-  (str "lein-zprint-1.2.8"))
+  (str "lein-zprint-1.2.9"))
 
 (defn zprint-about "Return version of zprint library program." [] (zc/about))
 
